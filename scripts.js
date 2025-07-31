10b187103da3cb24037553969cd6ab6edee25336
Joaquinemm <joaquinemendezmaeb@gmail.com>
2025-07-31 01:32:39 -0400

    Prueba

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function ShowHide() {
+    const modal = document.querySelector('.netflix-household');
+    modal.style.display = 'flex';
+}
+
+function Hide(){
+    const modal = document.querySelector('.netflix-household')
+    modal.style.display = 'none';
+}
